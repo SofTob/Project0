4014643b00eeeaf67977042b4eb3d8fd2bcb066a
sofiaburkow <63155724+sofiaburkow@users.noreply.github.com>
2023-09-13 21:18:40 +0200

    new setup branch created

---
+++ src/screens/countryList.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const CountryList = () => {
+    return (
+        <Text>Hello World!</Text>
+    );
+}
+
+export default CountryList;
